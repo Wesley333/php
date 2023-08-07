e4e30f870931ad352a1a57d238e110013d072387
Wesley Coelho dos Santos <86034843+Wesley333@users.noreply.github.com>
2023-08-07 12:51:09 -0300

    Update index.php

--- comparação de números/index.php
+++ comparação de números/index.php
@@ -9,34 +9,87 @@
 </head>
 
 <body>
-    <?php
-    echo '<h1>Observe os números abaixo:</h1>';
 
-    $n1 = 50;
-    $n2 = 50;
+    <form method="post">
+        <?php
+        echo '<input type="number" id="number1" name="number1" placeholder="Digite o primeiro número">';
+        echo '<br />';
+        echo '<input type="number" id="number2" name="number2" placeholder="Digite o segundo número">';
+        echo '<br>';
+        echo '<button type="submit">enviar</button>';
+        echo '<h1>Observe os números abaixo:</h1>';
 
-    echo 'Primeiro número é igual ah: ' . $n1;
-    echo '<br />';
-    echo 'Segundo número é igual ah: ' . $n2;
-    echo '<br />';
+        echo 'O Primeiro número é igual ah: ' . $_POST['number1'];
+        echo '<br />';
+        echo 'O Segundo número é igual ah: ' . $_POST['number2'];
+        echo '<br />';
 
-    echo '<h2>Agora vamos verificar se é o primeiro é maior ou menor que segundo:</h2>';
-    echo '<hr>';
-    echo '<h2>Resultado:</h2>';
+        echo '<h2>Agora vamos verificar se é o primeiro é maior ou menor que segundo:</h2>';
+        echo '<hr>';
+        echo '<h2>Resultado:</h2>';
 
-    if ($n1 > $n2) {
-        echo 'O primeiro número que é igual ah: ' . $n1 . ' é maior que o segundo número que é igual ah: ' . $n2;
-    } else if ($n1 < $n2) {
-        echo 'O segundo número que é igual ah: ' . $n2 . ' é maior que o primeiro número que é igual ah: ' . $n1;
-    } else {
-        echo 'O primeiro número é igual ah: ' . $n1 . ' e o segundo número também é igual ah: ' . $n2 . ' portanto são iguais.';
-    }
-    ?>
-    <script>
-        alert('Verificar se os números são iguais ou se o primeiro é maior que o segundo ou vice-versa')
-    </script>
-</body>
+        if ($_POST['number1'] > $_POST['number2']) {
+            $maior_numero = $_POST['number1'];
+            $menor_numero = $_POST['number2'];
+            echo 'O primeiro número que é igual ah: ' . $_POST['number1'] . ' é maior que o segundo número que é igual ah: ' . $_POST['number2'];
+        } else if ($_POST['number1'] < $_POST['number2']) {
+            $maior_numero = $_POST['number2'];
+            $menor_numero = $_POST['number1'];
+            echo 'O segundo número que é igual ah: ' . $_POST['number2'] . ' é maior que o primeiro número que é igual ah: ' . $_POST['number1'];
+        } else {
+            echo 'O primeiro número é igual ah: ' . $_POST['number1'] . ' e o segundo número também é igual ah: ' . $_POST['number2'] . ' portanto são iguais.';
+        }
 
+        echo '<hr>';
 
+        echo '<h2>A soma do $n1 mais o $n2: </h2>';
+        $soma = $_POST['number1'] + $_POST['number2'];
+        echo 'A soma dos números ' . $_POST['number1'] . '+' . $_POST['number2'] . ' eh igual ah:' . $soma;
+        echo '<br>';
+
+        echo '<hr>';
+        echo "<h2>Os números entre $menor_numero e $maior_numero em ordem crescente são:</h2>";
+        echo '<br>';
+
+        for ($i = $menor_numero + 1; $i < $maior_numero; $i++) {
+            echo $i;
+            if ($i < $maior_numero - 1) {
+                echo ', ';
+            }
+        }
+
+        echo '<hr>';
+        echo "<h2>Os números pares entre $menor_numero e $maior_numero em ordem crescente são:</h2>";
+        echo '<br>';
+
+        for ($i = $menor_numero + 1; $i < $maior_numero; $i++) {
+            if ($i % 2 == 0) {
+                echo $i;
+                if ($i < $maior_numero - 2) {
+                    echo ', ';
+                }
+            }
+        }
+
+        echo '<hr>';
+        echo "<h2 class='echo'>Os números</h2> <h2 class='fundoImpar';'>impares</h2> e <h2 class='fundoPar'>pares</h2> <h2 class='echo'>entre $menor_numero e $maior_numero em ordem crescente são:</h2>";
+        echo '<br>';
+
+        for ($i = $menor_numero + 1; $i < $maior_numero; $i++) {
+            if ($i % 2 != 0) {
+                echo "<div class='fundoImpar'>". $i ."</div>";
+                if ($i < $maior_numero - 1) {
+                    echo ', ';
+                }
+            }
+            if ($i % 2 == 0) {
+                echo "<div class='fundoPar'>". $i ."</div>";
+                if ($i < $maior_numero - 2) {
+                    echo ', ';
+                }
+            }
+        }
+        ?>
+    </body>
 
 </html>
