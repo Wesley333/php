1924b7190d5c8c74f215f8c49181bb3a1a8a21cd
Wesley Coelho dos Santos <86034843+Wesley333@users.noreply.github.com>
2023-08-07 14:36:05 -0300

    Update index.php

--- comparação de números/index.php
+++ comparação de números/index.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Teste de PHP</title>
+    <title>Programa em PHP</title>
     <link rel="stylesheet" href="style.css">
 </head>
 
